130c7b9e7fd06994c53ea6add29243e956ff991a
nchristene <christenekidd@gmail.com>
2025-02-12 17:24:43 -0800

    fix: remove imports TestimonialV1 and Partner V1 from innerPages/ServiceDetails.tsx

--- src/pages/innerPages/ServiceDetails.tsx
+++ src/pages/innerPages/ServiceDetails.tsx
@@ -3,9 +3,9 @@ import ServicesV1Data from "../../jsonData/services/ServicesV1Data.json"
 import ServiceDetailsContent from '../../components/services/ServiceDetailsContent';
 import FaqV1 from '../../components/faq/FaqV1';
 import PriceV1 from '../../components/pricing/PriceV1';
-import TestimonialV1 from '../../components/testimonial/TestimonialV1';
+//import TestimonialV1 from '../../components/testimonial/TestimonialV1';
 import ContactV1 from '../../components/contact/ContactV1';
-import PartnerV1 from '../../components/partner/PartnerV1';
+//import PartnerV1 from '../../components/partner/PartnerV1';
 import LayoutV3 from '../../components/layouts/LayoutV3';
 
 const ServiceDetailsPage = () => {
